d024c176d223f834b958ba09315c07b9565b6490
Imran Pasha Sinha <157057443+ImranPasha247@users.noreply.github.com>
2025-02-15 00:31:59 +0600

    Add more files of C++

---
+++ C++/Sheet 1/Task20.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double a;
+    cin>>a;    
+    if(a==(int)a){
+        cout<<"int "<<a<<endl;
+    }else{
+        cout<<"float "<<a<<endl;
+    }
+}
+// Q1. if(typeid(a).name()== "i"||"int"){ Const char, Comparison with string literal results in unspecified behavior[-Waddress]}
+// Q2. if(cout<<typeid(a).name()==cout<<"i")
+// Q3. if(a%1==0){boolean expression}
---
+++ C++/Sheet 1/Task23.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b,c,d,e;
+    cin>>a>>b>>c>>d;
+    e = a*b*c*d;
+    e = e%100;
+    cout<<e;
+}
